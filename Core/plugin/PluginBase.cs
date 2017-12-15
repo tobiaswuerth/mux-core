5d1e9c3da509ad5103bb5403b35b5515b90f9de7
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-12-15 12:30:18 +0100

    added plugin base

---
+++ Core/plugin/PluginBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ch.wuerth.tobias.mux.Core.logging;
+
+namespace ch.wuerth.tobias.mux.Core.plugin
+{
+    public abstract class PluginBase
+    {
+        private readonly LoggerBundle _logger;
+        public Boolean IsInitialized { get; set; }
+
+        protected PluginBase(LoggerBundle logger = null)
+        {
+            _logger = logger;
+        }
+
+        public Boolean Initialize(PluginConfigurator configurator)
+        {
+            try
+            {
+                ConfigurePlugin(configurator);
+                IsInitialized = true;
+            }
+            catch (Exception ex)
+            {
+                _logger?.Exception?.Log(ex);
+            }
+
+            return IsInitialized;
+        }
+
+        protected abstract void ConfigurePlugin(PluginConfigurator configurator);
+        protected abstract void Process(params String[] args);
+    }
+}
---
+++ Core/plugin/PluginConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ch.wuerth.tobias.mux.Core.plugin
+{
+    public class PluginConfigurator
+    {
+        public String Name { get; private set; }
+
+        public PluginConfigurator RegisterName(String name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            return this;
+        }
+    }
+}
