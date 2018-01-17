0a7d5f4a7a9ecec65bbce0146ced272e64a6d3dd
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-17 16:30:17 +0100

    created loggertype enum

---
+++ Core/logging/LoggerType.cs
@@ -0,0 +1,12 @@
+﻿namespace ch.wuerth.tobias.mux.Core.logging
+{
+    public enum LoggerType
+    {
+        Trace
+        , Debug
+        , Info
+        , Warning
+        , Error
+        , Fatal
+    }
+}
