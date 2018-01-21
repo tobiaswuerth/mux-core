f15ca25a7baf7813ae008cae26b0b539093fe031
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-21 22:06:26 +0100

    adjust file logger

--- Core/logging/logger/FileLogger.cs
+++ Core/logging/logger/FileLogger.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using ch.wuerth.tobias.mux.Core.processing;
 using ch.wuerth.tobias.ProcessPipeline;
+using global::ch.wuerth.tobias.mux.Core.global;
 
 namespace ch.wuerth.tobias.mux.Core.logging.logger
 {
@@ -7,9 +11,17 @@ namespace ch.wuerth.tobias.mux.Core.logging.logger
     {
         public FileLogger(LogTypes type) : base(type) { }
 
+        private String LogPath
+        {
+            get
+            {
+                return Path.Combine(Location.LogsDirectoryPath, $"mux_log_{Type}-{DateTime.Now:yy-MM-dd}.log");
+            }
+        }
+
         protected override ProcessPipe<String, String> GetExecuteLogPipe()
         {
-            throw new NotImplementedException();
+            return new WriterPipe(new StreamWriter(File.OpenWrite(LogPath), Encoding.UTF8));
         }
     }
 }
