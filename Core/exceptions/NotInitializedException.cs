4efaf5cf80e5015e06b7449b0f1e225691215c7f
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-21 17:47:50 +0100

    improve exceptions

--- Core/exceptions/NotInitializedException.cs
+++ Core/exceptions/NotInitializedException.cs
@@ -1,6 +1,16 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace ch.wuerth.tobias.mux.Core.exceptions
 {
-    public class NotInitializedException : Exception { }
+    public class NotInitializedException : Exception
+    {
+        public NotInitializedException() { }
+
+        public NotInitializedException(String message) : base(message) { }
+
+        public NotInitializedException(String message, Exception innerException) : base(message, innerException) { }
+
+        protected NotInitializedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
 }
