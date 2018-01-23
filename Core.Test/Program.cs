2dc1b20d5e617f4d0f88a0f797443db016111da3
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-23 06:49:04 +0100

    create test project (again) .. but this time as console application

---
+++ Core.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ch.wuerth.tobias.mux.Core.Test
+{
+    internal class Program
+    {
+        private static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
